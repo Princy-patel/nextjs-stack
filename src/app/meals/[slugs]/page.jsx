4824e4e461b2d45b855bef0dbc15e7b23ba9686d
Princy Patel <kapatelprincy@gmail.com>
2024-04-14 02:05:44 +0530

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- src/app/meals/[slugs]/page.jsx
+++ src/app/meals/[slugs]/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "../../../../lib/meals";
+import { notFound } from "next/navigation";
+async function BlogsPage({ params }) {
+  const meal = await getMeal(params.slugs);
 
-function BlogsPage({ params }) {
-  return <h1>{params.slugs}</h1>;
+  if (!meal) {
+    // will show closest not-found page or error page available in the project
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
 export default BlogsPage;
---
+++ src/app/meals/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, We could not find the requested page or meal data.</p>
+    </main>
+  );
+}
+
+export default NotFoundPage;
