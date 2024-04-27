122618fbf25f456bcdb0278b52167ddcded2340e
Princy Patel <kapatelprincy@gmail.com>
2024-04-27 23:05:28 +0530

    pref:optimize images with 'Next Image' compoenent & feature

--- nextEvents/components/events/event-item.jsx
+++ nextEvents/components/events/event-item.jsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import React from "react";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
+import Image from "next/image";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -19,7 +19,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={160}/>
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
