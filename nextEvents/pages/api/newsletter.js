3b945dd9f8cd961f65843840550d4abb4de20da7
Princy Patel <kapatelprincy@gmail.com>
2024-05-05 01:25:27 +0530

    perf:add error handling

--- nextEvents/pages/api/newsletter.js
+++ nextEvents/pages/api/newsletter.js
@@ -1,6 +1,20 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
+  async function connectDatabase() {
+    const client = await MongoClient.connect(
+      "mongodb+srv://kapatelprincy:Princy552001@cluster0.ka3fnuo.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    return client;
+  }
+
+  async function insertDocument(client, document) {
+    const db = client.db();
+
+    await db.collection("newsletter").insertOne(document);
+  }
+
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -9,15 +23,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://kapatelprincy:Princy552001@cluster0.ka3fnuo.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
-
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
     res.status(201).json({ message: "Signed up" });
   }
 }
